d923436613d1590ae380b217ea9d9e097359fcf7
mahfuzar148@gmail.com <mahfuzar148@gmail.com>
2023-08-26 23:43:33 +0600

    FileReader() and FileWriter Method has been created.

--- StudentList.java
+++ StudentList.java
@@ -2,56 +2,70 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
+
+	public static String FileReader()
+	{
+		String studentData = null;
+		try{
+			BufferedReader reader = new BufferedReader(
+					new InputStreamReader(
+							new FileInputStream("students.txt")));
+			studentData = reader.readLine();
+
+		}catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		return studentData;
+
+	}
+	public static void fileWriter(String content)
+	{
+		try{BufferedWriter writer = new BufferedWriter(
+				new FileWriter("students.txt", true));
+			writer.write(content);
+			writer.close();
+
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
 
 //		Check arguments
 		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String studentData = reader.readLine();
+			System.out.println("Loading data ...");
+			String studentData = FileReader();
 			String studentName[] = studentData.split(",");
 			for(String name : studentName) {
 				System.out.println(name);
-			}
-			} catch (Exception e){
-				System.out.println(e);
+
 			}
 			System.out.println("Data Loaded.");
 		}
 		else if(args[0].equals("r")) 
 		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String studentData = reader.readLine();
+			System.out.println("Loading data ...");
+			String studentData = FileReader();
 
 			String studentName[] = studentData.split(",");
 			Random random = new Random();
 			int index = random.nextInt(studentName.length);
 			System.out.println(studentName[index]);
 
-			} catch (Exception e){
-				System.out.println(e);
-			}
 			System.out.println("Data Loaded.");			
 		}
 		else if(args[0].contains("+")){
 			System.out.println("Loading data ...");			
 			try {
-			BufferedWriter writer = new BufferedWriter(
-					new FileWriter("students.txt", true));
+
 			String newStudentName = args[0].substring(1);
 	        Date currentDate = new Date();
 	        String dataFormatPattern = "dd/mm/yyyy-hh:mm:ss a";
 	        DateFormat dateFormat = new SimpleDateFormat(dataFormatPattern);
 	        String formattedDate= dateFormat.format(currentDate);
-			writer.write(", "+newStudentName+"\nList last updated on "+formattedDate);
-			writer.close();
+			fileWriter(", "+newStudentName+"\nList last updated on "+formattedDate);
 			} catch (Exception e){
 				System.out.println(e);
 			}
@@ -61,11 +75,8 @@ public class StudentList {
 		else if(args[0].contains("?")) 
 		{
 			System.out.println("Loading data ...");			
-			try {
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String studentData = reader.readLine();
+
+			String studentData = FileReader();
 			String studentName[] = studentData.split(",");
 			boolean found = false;
 			String t = args[0].substring(1);
@@ -75,19 +86,14 @@ public class StudentList {
 					found=true;
 				}
 			}
-			} catch (Exception e){
-				System.out.println(e);
-			}
+
 			System.out.println("Data Loaded.");				
 		}
 		else if(args[0].contains("c")) 
 		{
 			System.out.println("Loading data ...");			
-			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String studentData = s.readLine();
+
+			String studentData = FileReader();
 			char characters[] = studentData.toCharArray();
 			boolean in_word = false;
 			int wordCount=0;
@@ -104,9 +110,7 @@ public class StudentList {
 				}
 			}
 			System.out.println(wordCount +" word(s) found " );
-			} catch (Exception e){
-				System.out.println(e);
-			}
+
 			System.out.println("Data Loaded.");				
 		}
 		else {
